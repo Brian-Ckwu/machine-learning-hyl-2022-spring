61c46756f4d63f045a521bc5850d13d14247f844
ckwu <sonny40920710@gmail.com>
2022-02-19 18:02:19 +0800

    create a more complex model class: ComplexNN

--- homework/hw1/utils/model.py
+++ homework/hw1/utils/model.py
@@ -16,3 +16,22 @@ class SampleNN(nn.Module):
         x = self.layers(x)
         x = x.squeeze(1) # (B, 1) -> (B)
         return x
+
+class ComplexNN(nn.Module):
+    def __init__(self, input_dim):
+        super(ComplexNN, self).__init__()
+        # TODO: modify model's structure, be aware of dimensions. 
+        self.layers = nn.Sequential(
+            nn.Linear(input_dim, 32),
+            nn.ReLU(),
+            nn.Linear(32, 16),
+            nn.ReLU(),
+            nn.Linear(16, 8),
+            nn.ReLU(),
+            nn.Linear(8, 1)
+        )
+
+    def forward(self, x):
+        x = self.layers(x)
+        x = x.squeeze(1) # (B, 1) -> (B)
+        return x    
