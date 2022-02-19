6e4e78a599d3c61822fe0a1d8efe7b17bd880d34
ckwu <sonny40920710@gmail.com>
2022-02-19 15:41:35 +0800

    change trainer() utility function

--- homework/hw1/utils/funcs.py
+++ homework/hw1/utils/funcs.py
@@ -36,7 +36,7 @@ def select_feat(train_data, valid_data, test_data, select_all=True):
         
     return raw_x_train[:,feat_idx], raw_x_valid[:,feat_idx], raw_x_test[:,feat_idx], y_train, y_valid
 
-def trainer(train_loader, valid_loader, model, config, device):
+def trainer(train_loader, valid_loader, model, config, device, report_every_n_epochs):
 
     criterion = nn.MSELoss(reduction='mean') # Define your loss function, do not modify this.
 
@@ -50,57 +50,54 @@ def trainer(train_loader, valid_loader, model, config, device):
     if not os.path.isdir('./models'):
         os.mkdir('./models') # Create directory of saving models.
 
-    n_epochs, best_loss, step, early_stop_count = config['n_epochs'], math.inf, 0, 0
+    n_epochs, best_val_loss, step, early_stop_count = config['n_epochs'], math.inf, 0, 0
 
     for epoch in range(n_epochs):
         model.train() # Set your model to train mode.
-        loss_record = []
+        train_loss_record = list()
 
-        # tqdm is a package to visualize your training progress.
-        train_pbar = tqdm(train_loader, position=0, leave=True)
-
-        for x, y in train_pbar:
+        for x, y in train_loader:
             optimizer.zero_grad()               # Set gradient to zero.
             x, y = x.to(device), y.to(device)   # Move your data to device. 
             pred = model(x)             
-            loss = criterion(pred, y)
-            loss.backward()                     # Compute gradient(backpropagation).
+            train_loss = criterion(pred, y)
+            train_loss.backward()                     # Compute gradient(backpropagation).
             optimizer.step()                    # Update parameters.
             step += 1
-            loss_record.append(loss.detach().item())
-            
-            # Display current epoch number and loss on tqdm progress bar.
-            train_pbar.set_description(f'Epoch [{epoch+1}/{n_epochs}]')
-            train_pbar.set_postfix({'loss': loss.detach().item()})
+            train_loss_record.append(train_loss.detach().item())
 
-        mean_train_loss = sum(loss_record)/len(loss_record)
+        mean_train_loss = sum(train_loss_record)/len(train_loss_record)
         writer.add_scalar('Loss/train', mean_train_loss, step)
 
         model.eval() # Set your model to evaluation mode.
-        loss_record = []
+        val_loss_record = []
         for x, y in valid_loader:
             x, y = x.to(device), y.to(device)
             with torch.no_grad():
                 pred = model(x)
-                loss = criterion(pred, y)
+                val_loss = criterion(pred, y)
 
-            loss_record.append(loss.item())
-            
-        mean_valid_loss = sum(loss_record)/len(loss_record)
-        print(f'Epoch [{epoch+1}/{n_epochs}]: Train loss: {mean_train_loss:.4f}, Valid loss: {mean_valid_loss:.4f}')
-        writer.add_scalar('Loss/valid', mean_valid_loss, step)
+            val_loss_record.append(val_loss.item())
+        
 
-        if mean_valid_loss < best_loss:
-            best_loss = mean_valid_loss
+        mean_val_loss = sum(val_loss_record)/len(val_loss_record)
+        writer.add_scalar('Loss/valid', mean_val_loss, step)
+        if (epoch + 1) % report_every_n_epochs == 0:
+            print(f'Epoch [{epoch+1}/{n_epochs}]: Train loss: {mean_train_loss:.4f}, Valid loss: {mean_val_loss:.4f}')
+
+        if mean_val_loss < best_val_loss:
+            best_val_loss = mean_val_loss
             torch.save(model.state_dict(), config['save_path']) # Save your best model
-            print('Saving model with loss {:.3f}...'.format(best_loss))
+            print('Saving model with loss {:.3f}...'.format(best_val_loss))
             early_stop_count = 0
         else: 
             early_stop_count += 1
 
         if early_stop_count >= config['early_stop']:
             print('\nModel is not improving, so we halt the training session.')
-            return
+            break
+    
+    return best_val_loss, epoch
 
 def predict(test_loader, model, device):
     model.eval() # Set your model to evaluation mode.
