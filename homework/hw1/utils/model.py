1f41f30e2eae420b73bd6d4b0769a2f2cf88128d
ckwu <sonny40920710@gmail.com>
2022-02-21 10:50:19 +0800

    add new models

--- homework/hw1/utils/model.py
+++ homework/hw1/utils/model.py
@@ -1,5 +1,39 @@
 from torch import nn
 
+class DeepNN(nn.Module):
+    def __init__(self, input_dim):
+        super(DeepNN, self).__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(input_dim, 64),
+            nn.ReLU(),
+            nn.Linear(64, 64),
+            nn.ReLU(),
+            nn.Linear(64, 64),
+            nn.ReLU(),
+            nn.Linear(64, 64),
+            nn.ReLU(),
+            nn.Linear(64, 1)
+        )
+    
+    def forward(self, x):
+        x = self.layers(x)
+        x = x.squeeze(1)
+        return x    
+
+class SimpleNN(nn.Module):
+    def __init__(self, input_dim):
+        super(SimpleNN, self).__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(input_dim, 8),
+            nn.ReLU(),
+            nn.Linear(8, 1)
+        )
+    
+    def forward(self, x):
+        x = self.layers(x)
+        x = x.squeeze(1)
+        return x
+
 class SampleNN(nn.Module):
     def __init__(self, input_dim):
         super(SampleNN, self).__init__()
@@ -34,4 +68,25 @@ class ComplexNN(nn.Module):
     def forward(self, x):
         x = self.layers(x)
         x = x.squeeze(1) # (B, 1) -> (B)
-        return x    
+        return x
+
+class MoreComplexNN(nn.Module):
+    def __init__(self, input_dim):
+        super(MoreComplexNN, self).__init__()
+        # TODO: modify model's structure, be aware of dimensions. 
+        self.layers = nn.Sequential(
+            nn.Linear(input_dim, 64),
+            nn.ReLU(),
+            nn.Linear(64, 32),
+            nn.ReLU(),
+            nn.Linear(32, 16),
+            nn.ReLU(),
+            nn.Linear(16, 8),
+            nn.ReLU(),
+            nn.Linear(8, 1)
+        )
+
+    def forward(self, x):
+        x = self.layers(x)
+        x = x.squeeze(1) # (B, 1) -> (B)
+        return x  
