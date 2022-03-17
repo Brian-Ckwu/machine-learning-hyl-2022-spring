c20f7b4b7c350d33c2c20673fae787aee3d14d3f
ckwu <sonny40920710@gmail.com>
2022-03-17 23:56:36 +0800

    add LibriRNNDataset

--- homework/hw2/data.py
+++ homework/hw2/data.py
@@ -1,3 +1,4 @@
+from multiprocessing.sharedctypes import Value
 import random
 import torch
 import os
@@ -5,6 +6,7 @@ import os
 from tqdm import tqdm
 from torch.utils.data import Dataset
 from typing import List, Dict
+from torch.nn.utils.rnn import pad_sequence, pack_padded_sequence
 
 class LibriDataset(Dataset):
     def __init__(self, X, y=None):
@@ -25,7 +27,7 @@ class LibriDataset(Dataset):
 
 # return (feats, labels) of a single audio sequence
 class LibriRNNDataset(Dataset):
-    def __init__(self, audio_list_path: str = "./libriphone", audio_feat_path: str = "./libriphone/feat", mode: str = "train", train_ratio: float = 0.80, train_val_seed: int = 1337):
+    def __init__(self, audio_list_path: str = "./libriphone", audio_feat_path: str = "./libriphone/feat", concat_nframes: int = 11, mode: str = "train", train_ratio: float = 0.80, train_val_seed: int = 1337):
         # store variables
         assert mode in ["train", "val", "test"]
         self.mode = mode
@@ -48,6 +50,8 @@ class LibriRNNDataset(Dataset):
             if mode != "test":
                 labels = torch.LongTensor(label_dict[fname])
                 self.y.append(labels)
+        # TODO: concat feats -> not effective
+        # self.x = self.concat_feats(self.x, concat_nframes)
         if self.y:
             assert len(self.x) == len(self.y)
         
@@ -81,6 +85,44 @@ class LibriRNNDataset(Dataset):
                 label_dict[line[0]] = [int(l) for l in line[1:]]
         return label_dict
 
+    @staticmethod
+    def concat_feats(audios, concat_nframes: int) -> list:
+        # utility functions
+        def get_left(audio, idx, half_window):
+            left_idx = idx - half_window
+            if left_idx < 0:
+                padding_feats = audio[0].repeat(-left_idx)
+                left_feats = torch.cat(tensors=(padding_feats, audio[:idx].flatten()))
+            else:
+                left_feats = audio[left_idx:idx].flatten()
+            return left_feats
+        
+        def get_right(audio, idx, half_window):
+            last_idx = len(audio) - 1
+            right_idx = idx + half_window
+            if right_idx > last_idx:
+                padding_feats = audio[last_idx].repeat(right_idx - last_idx)
+                right_feats = torch.cat(tensors=(padding_feats, audio[idx:].flatten()))
+            else:
+                right_feats = audio[idx:right_idx + 1].flatten()
+            return right_feats        
+
+        assert concat_nframes % 2 == 1
+        half_window = concat_nframes // 2
+
+        converted_audios = list()
+        for audio in tqdm(audios):
+            converted_audio = list()
+            for idx, frame in enumerate(audio):
+                left_frames = get_left(audio, idx, half_window)
+                right_frames = get_right(audio, idx, half_window)
+                frames = torch.cat(tensors=(left_frames, right_frames))
+                converted_audio.append(frames)
+            converted_audio = torch.stack(converted_audio, dim=0)
+            converted_audios.append(converted_audio)
+        
+        return converted_audios
+
 class BatchCollator(object):
     def __init__(self, concat_nframes, feature_dim, test_mode: bool = False):
         self.nframes = concat_nframes
@@ -106,3 +148,31 @@ class BatchCollator(object):
         batch_x = torch.stack(tensors=batch_x)
         batch_y = torch.LongTensor(batch_y)
         return batch_x, batch_y
+
+class RNNCollator(object):
+    def __init__(self, mode):
+        self.mode = mode
+    
+    def __call__(self, batch):
+        if self.mode == "train":
+            X = list()
+            Y = list()
+            seq_lens = list()
+            for x, y in batch:
+                X.append(x)
+                Y.append(y)
+                seq_lens.append(len(x))
+            Y = pad_sequence(Y, batch_first=True)
+            Y = pack_padded_sequence(Y, seq_lens, batch_first=True, enforce_sorted=False).data
+        elif self.mode == "test":
+            X = list()
+            seq_lens = list()
+            for x in batch:
+                X.append(x)
+                seq_lens.append(len(x))
+        else:
+            raise ValueError("mode must be train or test")
+
+        X = pad_sequence(X, batch_first=True)
+        X = pack_padded_sequence(X, seq_lens, batch_first=True, enforce_sorted=False)
+        return (X, Y) if (self.mode != "test") else X
