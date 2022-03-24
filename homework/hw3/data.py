c68b194ed82ec5aeba8526566bd26707b9bf8c93
ckwu <sonny40920710@gmail.com>
2022-03-24 23:47:02 +0800

    add transforms

--- homework/hw3/data.py
+++ homework/hw3/data.py
@@ -37,10 +37,76 @@ test_tfm = transforms.Compose([
 
 # However, it is also possible to use augmentation in the testing phase.
 # You may use train_tfm to produce a variety of images and then test using ensemble methods
-train_tfm = transforms.Compose([
-    # Resize the image into a fixed shape (height = width = 128)
+# train_tfm = transforms.Compose([
+#     # Resize the image into a fixed shape (height = width = 128)
+#     transforms.Resize((128, 128)),
+#     # TODO: add some transforms here.
+#     transforms.RandomAffine(degrees=45, translate=(0.2, 0.2), scale=(0.75, 1.25)),
+#     transforms.RandomHorizontalFlip(p=0.5),
+#     # transforms.RandomChoice(transforms=[
+#     #     transforms.RandomAdjustSharpness(sharpness_factor=0, p=0.5),
+#     #     transforms.RandomAdjustSharpness(sharpness_factor=2, p=0.5)
+#     # ]),
+#     # transforms.RandomVerticalFlip(p=0.5),
+#     transforms.ToTensor()
+# ])
+
+horizontal_tfm = transforms.Compose([
     transforms.Resize((128, 128)),
-    # TODO: You may add some transforms here.
-    # ToTensor() should be the last one of the transforms.
-    transforms.ToTensor(),
+    transforms.RandomHorizontalFlip(p=0.5),
+    transforms.ToTensor()
 ])
+
+vertical_tfm = transforms.Compose([
+    transforms.Resize((128, 128)),
+    transforms.RandomVerticalFlip(p=0.5),
+    transforms.ToTensor()
+])
+
+affine_tfm = transforms.Compose([
+    transforms.Resize((128, 128)),
+    transforms.RandomAffine(degrees=90, translate=(0.3, 0.3), scale=(0.75, 1.25)),
+    transforms.ToTensor()
+])
+
+blurred_tfm = transforms.Compose([
+    transforms.Resize((128, 128)),
+    transforms.RandomAdjustSharpness(sharpness_factor=0, p=0.5),
+    transforms.ToTensor()
+])
+
+sharp_tfm = transforms.Compose([
+    transforms.Resize((128, 128)),
+    transforms.RandomAdjustSharpness(sharpness_factor=2, p=0.5),
+    transforms.ToTensor()
+])
+
+color_tfm = transforms.Compose([
+    transforms.Resize((128, 128)),
+    transforms.ColorJitter(brightness=0.10, contrast=0.10, saturation=0.10, hue=0.10),
+    transforms.ToTensor()
+])
+
+perspective_tfm = transforms.Compose([
+    transforms.Resize((128, 128)),
+    transforms.RandomPerspective(distortion_scale=0.5, p=0.5),
+    transforms.ToTensor()
+])
+
+crop_tfm = transforms.Compose([
+    transforms.Resize((128, 128)),
+    transforms.RandomResizedCrop(size=(128, 128), scale=(0.5, 1.0)),
+    transforms.ToTensor()
+])
+
+tfm_mapping = {
+    "horizontal": horizontal_tfm,
+    "vertical": vertical_tfm,
+    "affine": affine_tfm,
+    "blurred": blurred_tfm,
+    "sharp": sharp_tfm,
+    "color": color_tfm,
+    "perspective": perspective_tfm,
+    "crop": crop_tfm,
+    "test": test_tfm
+}
