82c4779bad540bd7f6c10cbaf6728a670b064274
ckwu <sonny40920710@gmail.com>
2022-03-24 11:29:15 +0800

    complete initial data.py file

---
+++ homework/hw3/data.py
@@ -0,0 +1,46 @@
+import os
+from PIL import Image
+
+import torchvision.transforms as transforms
+from torch.utils.data import Dataset
+
+class FoodDataset(Dataset):
+    def __init__(self, path, tfm, files=None):
+        self.path = path
+        if files:
+            self.files = files
+        else:
+            self.files = sorted([os.path.join(path, x) for x in os.listdir(path) if x.endswith(".jpg")])
+        print(f"One {path} sample",self.files[0])
+        self.transform = tfm
+  
+    def __len__(self):
+        return len(self.files)
+  
+    def __getitem__(self,idx):
+        fname = self.files[idx]
+        im = Image.open(fname)
+        im = self.transform(im)
+        #im = self.data[idx]
+        try:
+            label = int(fname.split("/")[-1].split("_")[0])
+        except:
+            label = -1 # test has no label
+        return im, label
+
+# Normally, We don't need augmentations in testing and validation.
+# All we need here is to resize the PIL image and transform it into Tensor.
+test_tfm = transforms.Compose([
+    transforms.Resize((128, 128)),
+    transforms.ToTensor(),
+])
+
+# However, it is also possible to use augmentation in the testing phase.
+# You may use train_tfm to produce a variety of images and then test using ensemble methods
+train_tfm = transforms.Compose([
+    # Resize the image into a fixed shape (height = width = 128)
+    transforms.Resize((128, 128)),
+    # TODO: You may add some transforms here.
+    # ToTensor() should be the last one of the transforms.
+    transforms.ToTensor(),
+])
