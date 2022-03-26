73cb73c955863ccfe13bd5f77d8767d9e175ce6f
ckwu <sonny40920710@gmail.com>
2022-03-26 09:29:22 +0800

    add Residual_Model

--- homework/hw3/model.py
+++ homework/hw3/model.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+from torchvision.models import resnet18
 
 class SampleClassifier(nn.Module):
     def __init__(self):
@@ -45,6 +46,80 @@ class SampleClassifier(nn.Module):
         out = out.view(out.size()[0], -1)
         return self.fc(out)
 
+class Residual_Network(nn.Module):
+    def __init__(self):
+        super(Residual_Network, self).__init__()
+        self.cnn_layer1 = nn.Sequential(
+            nn.Conv2d(3, 64, 3, 1, 1),
+            nn.BatchNorm2d(64),
+        )
+        self.cnn_layer2 = nn.Sequential(
+            nn.Conv2d(64, 64, 3, 1, 1),
+            nn.BatchNorm2d(64),
+        )
+        self.cnn_layer3 = nn.Sequential(
+            nn.Conv2d(64, 128, 3, 2, 1),
+            nn.BatchNorm2d(128),
+        )
+        self.cnn_layer4 = nn.Sequential(
+            nn.Conv2d(128, 128, 3, 1, 1),
+            nn.BatchNorm2d(128),
+        )
+        self.cnn_layer5 = nn.Sequential(
+            nn.Conv2d(128, 256, 3, 2, 1),
+            nn.BatchNorm2d(256),
+        )
+        self.cnn_layer6 = nn.Sequential(
+            nn.Conv2d(256, 256, 3, 1, 1),
+            nn.BatchNorm2d(256),
+        )
+        self.fc_layer = nn.Sequential(
+            nn.Linear(256* 32* 32, 256),
+            nn.ReLU(),
+            nn.Linear(256, 11)
+        )
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        # input (x): [batch_size, 3, 128, 128]
+        # output: [batch_size, 11]
+        # Extract features by convolutional layers.
+        z1 = self.cnn_layer1(x)
+        a1 = self.relu(z1)
+
+        z2 = self.cnn_layer2(a1)
+        z2_res = a1 + z2
+        a2 = self.relu(z2_res)
+
+        z3 = self.cnn_layer3(a2)
+        a3 = self.relu(z3)
+
+        z4 = self.cnn_layer4(a3)
+        z4_res = a3 + z4
+        a4 = self.relu(z4_res)
+
+        z5 = self.cnn_layer5(a4)
+        a5 = self.relu(z5)
+
+        z6 = self.cnn_layer6(a5)
+        z6_res = a5 + z6
+        a6 = self.relu(z6_res)
+
+        xout = a6.flatten(1)
+        xout = self.fc_layer(xout)
+        return xout
+
+class ResNet18Classifier(nn.Module):
+    def __init__(self):
+        super(ResNet18Classifier, self).__init__()
+        self.resnet18 = resnet18(pretrained=False, num_classes=11)
+    
+    def forward(self, x):
+        out = self.resnet18(x)
+        return out
+
 model_mapping = {
-    "SampleClassifier": SampleClassifier
+    "SampleClassifier": SampleClassifier,
+    "ResNet18Classifier": ResNet18Classifier,
+    "Residual_Network": Residual_Network
 }
