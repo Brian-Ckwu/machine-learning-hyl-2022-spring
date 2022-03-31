11e7d97685f7c32cff740a7bd3fa5b0fb93b65a1
ckwu <sonny40920710@gmail.com>
2022-03-31 23:46:41 +0800

    modify test.py

--- homework/hw4/test.py
+++ homework/hw4/test.py
@@ -9,7 +9,7 @@ import torch
 from torch.utils.data import DataLoader
 
 from data import InferenceDataset, inference_collate_batch
-from model import SampleClassifier
+from model import model_mapping
 from utils import load_config
 
 def tester(args: Namespace):
@@ -20,19 +20,19 @@ def tester(args: Namespace):
 	mapping_path = Path(args.data_dir) / "mapping.json"
 	mapping = json.loads(mapping_path.read_bytes())
 
-	dataset = InferenceDataset(args.data_dir)
+	dataset = InferenceDataset(args.data_dir, segment_len=args.segment_len)
 	dataloader = DataLoader(
 		dataset,
 		batch_size=1,
 		shuffle=False,
 		drop_last=False,
-		num_workers=8,
+		num_workers=4,
 		collate_fn=inference_collate_batch,
 	)
 	print(f"[Info]: Finish loading data!")
 
 	speaker_num = len(mapping["id2speaker"])
-	model = SampleClassifier(args, n_spks=speaker_num).to(args.device)
+	model = model_mapping[args.model](args, n_spks=speaker_num).to(args.device)
 	model.load_state_dict(torch.load(Path(args.model_dir) / args.exp_name / "model.pth"))
 	model.eval()
 	print(f"[Info]: Finish creating model!")
@@ -42,7 +42,7 @@ def tester(args: Namespace):
 		with torch.no_grad():
 			mels = mels.to(args.device)
 			outs = model(mels)
-			preds = outs.argmax(1).cpu().numpy()
+			preds = outs.argmax(dim=-1).cpu().numpy()
 			for feat_path, pred in zip(feat_paths, preds):
 				results.append([feat_path, mapping["id2speaker"][str(pred)]])
 
