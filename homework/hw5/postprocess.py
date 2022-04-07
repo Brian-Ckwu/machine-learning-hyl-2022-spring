70ff92891deaa017e89039faca0c0d2874d55bcb
ckwu <sonny40920710@gmail.com>
2022-04-07 15:49:57 +0800

    add remove space function

--- homework/hw5/postprocess.py
+++ homework/hw5/postprocess.py
@@ -1,15 +1,16 @@
 import re
 import sys
 
-def remove_unk_and_save(in_file: str, out_file: str) -> None:
+def clean_and_save(in_file: str, out_file: str) -> None:
     with open(in_file, "rt") as f_in:
         with open(out_file, "wt") as f_out:
             for line_in in f_in:
                 line_out = re.sub(pattern=r"<unk>", repl="", string=line_in)
+                line_out = re.sub(pattern=r" ", repl="", string=line_out)
                 f_out.write(line_out)
 
 if __name__ == "__main__":
     # post processing code
     in_file = sys.argv[1]
     out_file = sys.argv[2]
-    remove_unk_and_save(in_file, out_file)
+    clean_and_save(in_file, out_file)
